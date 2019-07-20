6085a25c4e5fb73826b2d9a6394f5ca72a25b8eb
tifa.zsc <tifa.zsc@gmail.com>
2019-07-20 23:44:12 +0200

    Add market screen

--- src/Components/Market.jsx
+++ src/Components/Market.jsx
@@ -1,17 +1,53 @@
 import React, { Component } from "react";
 class Market extends Component {
   state = {
-    loading: true,
-    Stocks: []
+    loading: false,
+    stocks: []
   };
   async componentDidMount() {
-    const url = "http://localhost:60713/api/stocks";
-    const response = await fetch(url);
-    const data = response.json();
-    console.log(data);
+    await fetch("http://localhost:60713/api/stocks")
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          loading: true,
+          stocks: json
+        });
+      });
+    // const url = "http://localhost:60713/api/stocks";
+    // const response = await fetch(url);
+    // const data = response.json();
+    // this.setState({ stocks: data });
+    // console.log(this.state.stocks);
   }
   render() {
-    return <h1>Market</h1>;
+    let i = 0;
+    if (!this.state.loading) {
+      return <div>Loading Data </div>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Market</h1>
+          <table className="table  table-bordered table-dark table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Stock Name</th>
+                <th scope="col">Price $</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.stocks.map(stock => (
+                <tr key={stock.stockId}>
+                  <th scope="row">{(i = i + 1)}</th>
+                  <td>{stock.name}</td>
+                  <td>{stock.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+    }
   }
 }
 
