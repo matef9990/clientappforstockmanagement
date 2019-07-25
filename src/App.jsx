3edfafda5edb3038c188d03d61b563a5b772ec99
Mohamed Atef <m.atef9990@gmail.com>
2019-07-25 09:19:04 +0200

    Send data to market by props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { HubConnectionBuilder } from "@aspnet/signalr";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./Components/navbar";
 import logo from "./logo.svg";
@@ -10,15 +11,51 @@ import http from "./Services/httpServices";
 import config from "./config.json";
 
 class App extends Component {
-  state = {};
+  state = {
+    loading: false,
+    stocks: [],
+    hubConnection: null
+  };
   async componentDidMount() {
     //Call api Interval every 10 sec
+
+    // Get All Stocks From Database by Web Api
+    await http.get(config.endPointUrl).then(response => {
+      this.setState({
+        loading: true,
+        stocks: response.data
+      });
+    });
 
     setInterval(async () => {
       http.patch(config.endPointUrl);
     }, 10000);
+
+    const hubConnection = new HubConnectionBuilder()
+      .withUrl(config.signalrHubUrl)
+      .build();
+
+    this.setState({ hubConnection }, () => {
+      this.state.hubConnection
+        .start()
+        .then(() => console.log("SignalR Connected"))
+        .catch(err => console.log("Error in Connecting SignalR" + err));
+
+      hubConnection.on("StocksList_Refreshed", () => {
+        this.refreshMarket();
+      });
+    });
   }
 
+  async refreshMarket() {
+    await http.get(config.endPointUrl).then(response => {
+      this.setState({
+        stocks: response.data
+      });
+    });
+
+    console.log("data refreshed");
+  }
   render() {
     return (
       <React.Fragment>
@@ -26,7 +63,16 @@ class App extends Component {
 
         <main className="container mt-5">
           <Switch>
-            <Route path="/market" component={Market} />
+            <Route
+              path="/market"
+              render={props => (
+                <Market
+                  {...props}
+                  loading={this.state.loading}
+                  stocks={this.state.stocks}
+                />
+              )}
+            />
             <Route path="/orders" component={Orders} />
             <Route path="/notFound" component={NotFound} />
             <Redirect path="/" exact to="/market" />
--- src/Components/Market.jsx
+++ src/Components/Market.jsx
@@ -4,65 +4,9 @@ import http from "../Services/httpServices";
 import config from "../config.json";
 
 class Market extends Component {
-  _isMounted = false;
-  state = {
-    loading: false,
-    stocks: [],
-    hubConnection: null
-  };
-  async componentDidMount() {
-    this._isMounted = true;
-
-    // Get All Stocks From Database by Web Api
-    if (this._isMounted) {
-      await http.get(config.endPointUrl).then(response => {
-        this.setState({
-          loading: true,
-          stocks: response.data
-        });
-      });
-    }
-
-    // connect to Hub to refresh DataBase
-
-    const hubConnection = new HubConnectionBuilder()
-      .withUrl(config.signalrHubUrl)
-      .build();
-
-    this.setState({ hubConnection }, () => {
-      this.state.hubConnection
-        .start()
-        .then(() => console.log("SignalR Connected"))
-        .catch(err => console.log("Error in Connecting SignalR" + err));
-
-      hubConnection.on("StocksList_Refreshed", () => {
-        this.refreshMarket();
-      });
-    });
-  }
-
-  //comoponent will not mount
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  //do when database refreshed
-  async refreshMarket() {
-    if (this._isMounted) {
-      await http.get(config.endPointUrl).then(response => {
-        this.setState({
-          stocks: response.data
-        });
-      });
-
-      console.log("data refreshed");
-    }
-  }
-
   render() {
     let i = 0;
-    if (!this.state.loading) {
+    if (!this.props.loading) {
       return <div>Loading Data ... </div>;
     } else {
       return (
@@ -79,7 +23,7 @@ class Market extends Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.stocks.map(stock => (
+              {this.props.stocks.map(stock => (
                 <tr key={stock.stockId}>
                   <th scope="row">{(i = i + 1)}</th>
                   <td>{stock.name}</td>
